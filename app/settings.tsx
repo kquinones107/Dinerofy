e22f6790ed37606a4dd48e804ba2ec8d8007767e
kquinones107 <kquinones@unal.edu.co>
2025-02-22 09:03:38 -0500

    feature: add screen of setting

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,12 +1,88 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import COLORS from '../components/theme';
+import { router } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
-const settings = () => {
+
+export default function SettingsScreen() {
+  
+
   return (
-    <View>
-      <Text>settings</Text>
+    <View style={styles.container}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+      <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+        <Ionicons name="arrow-back" size={24} color={COLORS.white} style={{ marginBottom: 20 }} />
+      </TouchableOpacity>
+      
+      <Text style={styles.title}>Configuración</Text>
+      </View>
+
+      <TouchableOpacity style={styles.menuItem}>
+        <Text style={styles.menuText}>👤 Editar Perfil</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.menuItem}>
+        <Text style={styles.menuText}>🔒 Seguridad</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.menuItem}>
+        <Text style={styles.menuText}>👤 Mi Información</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.menuItem}>
+        <Text style={styles.menuText}>💬 Centro de Ayuda</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.logoutButton}>
+        <Text style={styles.logoutText}>🚪 Cerrar Sesión</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
-export default settings
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.primary,
+    padding: 20,
+  },
+  backButton: {
+    marginBottom: 10,
+  },
+  backButtonText: {
+    color: COLORS.accent,
+    fontSize: 18,
+  },
+  title: {
+    fontSize: 24,
+    color: COLORS.accent,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 30,
+    marginRight: 120,
+  },
+  menuItem: {
+    backgroundColor: COLORS.secondary,
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 15,
+    alignItems: 'center',
+  },
+  menuText: {
+    fontSize: 18,
+    color: COLORS.white,
+  },
+  logoutButton: {
+    backgroundColor: 'red',
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  logoutText: {
+    fontSize: 18,
+    color: COLORS.white,
+  },
+});
+
